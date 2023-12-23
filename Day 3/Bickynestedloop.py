909cdc203a3decad9643f25da859df325b7eb0bf
Binaya Kumar Barick <154009601+BinayaKumarBarick@users.noreply.github.com>
2023-12-23 16:33:51 +0530

    Update and rename Bicky.py to Bickynestedloop.py

---
+++ Day 3/Bickynestedloop.py
@@ -0,0 +1,9 @@
+lst=[]
+limit=int(input("Enter Your limit: "))
+for i in range(limit):
+    for j in range(2):
+        for k in range(3):
+             lst.append(input())
+              
+print(lst)
+
