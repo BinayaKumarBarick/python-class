2a1f8459b61e5a01e46b0809cd469ec4ee59c854
Binaya Kumar Barick <154009601+BinayaKumarBarick@users.noreply.github.com>
2024-01-04 12:28:53 +0530

    Update class.py

--- Day5/class.py
+++ Day5/class.py
@@ -1,28 +1,33 @@
 class Item:
-    def _init_(self,name,type,size,others):
-        self.name=name
-        self.type=type
-        self.others=others
-        self.calculate_size()
+    def __init__(self, name, type, others):
+        self.name = name
+        self.type = type
+        self.others = others 
+        self.size=self.calculate_size()
 
     def calculate_size(self):
-        self.size=len(self.name)+len(self.type)
+        self.size = len(self.name) + len(self.type)
+        return self.size
 
-item ={
-        "name":"laptop", "type":"electronics","others":{"brand":"xyz","color":"silver"}
-      }
-print(item.name,item.type,item.others)
+class Storage:
+    def __init__(self, host, port, protocol ):
+        self.host = host
+        self.port = port
+        self.protocol = protocol
+        self.container = []
 
-
-class storage:
-    def _init_(self,host,port,protocol,container):
-        self.host=host
-        self.port=port
-        self.protocol=protocol
-        self.container=[]
-
-    def add_element(self,item):
-         self.container.append(item)
+    def add_element(self, item):
+        self.container.append(item)
+        return item
 
     def calculate_size(self):
-         return sum(item.size for item in self.container)
+        return sum(item.size for item in self.container)
+    
+item1= Item("laptop","electronics",others={ "color":"silver","brand":"xyz"})
+print(item1.size)
+storage1=Storage("host1",22,"http")
+print(storage1.host)
+print(storage1.add_element(item1))
+       
+
+
