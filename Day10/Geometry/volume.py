69f120c015e34d33c5f5bd658d4b87f52b8a14d0
Binaya Kumar Barick <154009601+BinayaKumarBarick@users.noreply.github.com>
2024-01-16 13:43:08 +0530

    Create volume.py

---
+++ Day10/Geometry/volume.py
@@ -0,0 +1,9 @@
+'''
+This module is used volume of various Geometrical shapes'''
+def cube(s):
+    '''find volume of cube s*s*s'''
+    return s*s*s
+
+def cuboid(l,w,h):
+    ''' find volume of cuboid'''
+    return l*w*h
