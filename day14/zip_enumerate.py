73e84a5acbb285b0a4c38960112988d0d3fec360
Binaya Kumar Barick <154009601+BinayaKumarBarick@users.noreply.github.com>
2024-01-25 09:26:33 +0530

    Create zip_enumerate.py

---
+++ day14/zip_enumerate.py
@@ -0,0 +1,5 @@
+names=['Rocky','Lucky','Hari','Rohan','Liza']
+subjects=['python','c','java','cloudcomputing','csa']
+marks=[78,82,100,95,75]
+for i,t  in  enumerate(zip(names,subjects,marks)):
+    print(i,t)
